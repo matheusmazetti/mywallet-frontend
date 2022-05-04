d8862f0ac89f7ce10ae887afcde38c76392a7480
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-04 11:27:54 -0300

    reset css

---
+++ src/App/Login/LoginStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+    width: 100%;
+    background: #8C11BE;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1{
+        
+    }
+`;
