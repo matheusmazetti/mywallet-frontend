f57c354250fff348ec5b0c6fbebee6b7b4895e9f
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-08 14:15:16 -0300

    registro completo

--- src/App/Register/Register.js
+++ src/App/Register/Register.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import * as S from './RegisterStyles';
+import axios from 'axios';
 
 export default function Register(){
     const [name, setName] = React.useState('');
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [validation, setValidation] = React.useState('');
+    let navigate = useNavigate();
+
+    function sendRegister(){
+        if(name !== '' && email !== '' && password !== '' && password === validation){
+            let obj = {
+                user: name,
+                email: email,
+                password: password
+            };
+            let promisse = axios.post('http://localhost:5000/register', obj);
+            promisse.then(() => {
+                navigate('/');
+            })
+            promisse.catch((e) => console.log(e));
+        } else {
+            alert('Preencha os campos corretamente');
+        }
+    }
     return(
         <S.Content>
             <h1>MyWallet</h1>
@@ -14,7 +33,7 @@ export default function Register(){
             <input placeholder="E-Mail" type='email' value={email} onChange={e => setEmail(e.target.value)}></input>
             <input placeholder="Senha" type='password' value={password} onChange={e => setPassword(e.target.value)}></input>
             <input placeholder="Confirme a senha" type='password' value={validation} onChange={e => setValidation(e.target.value)}></input>
-            <button>Cadastrar</button>
+            <button onClick={sendRegister}>Cadastrar</button>
             <Link to='/'>Já tem uma conta? Entre agora!</Link>
         </S.Content>
     )
