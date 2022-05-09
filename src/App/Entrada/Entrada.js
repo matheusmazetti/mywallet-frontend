e77439d2e681703daff393c14b91d1c0b32cf5d7
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-09 00:35:20 -0300

    entrada e saida funcionais

--- src/App/Entrada/Entrada.js
+++ src/App/Entrada/Entrada.js
@@ -1,13 +1,38 @@
 import * as S from './EntradaStyles';
-import React from 'react'; 
+import React from 'react';
+import Context from '../Context';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; 
 
 export default function Entrada(){
+    const [value, setValue] = React.useState(0);
+    const [name, setName] = React.useState('');
+    const context = React.useContext(Context);
+    const navigate = useNavigate();
+    function sendTask(){
+        if(value !== 0 && name !== ''){
+            console.log(context.user);
+            let obj = {
+                email: context.user.email,
+                token: context.user.token,
+                name: name,
+                type: "entrada",
+                value: value
+            }
+            let promisse = axios.post("http://localhost:5000/task", obj);
+            promisse.then(() => navigate('/wallet'));
+            promisse.catch(() => alert('Erro. Tente novamente!'));
+        } else {
+            alert('Preencha todos os campos');
+        }
+    }
+
     return(
         <S.Content>
             <h1>Nova entrada</h1>
-            <input placeholder="Valor"></input>
-            <input placeholder="Descrição"></input>
-            <button>Salvar entrada</button>
+            <input placeholder="Valor" type="number" value={value} onChange={e => setValue(e.target.value)}></input>
+            <input placeholder="Descrição" type="text" value={name} onChange={e => setName(e.target.value)}></input>
+            <button onClick={sendTask}>Salvar entrada</button>
         </S.Content>
     )
 }
--- src/App/Saida/Saida.js
+++ src/App/Saida/Saida.js
@@ -1,12 +1,37 @@
-import * as S from './SaidaStyles'; 
+import * as S from './SaidaStyles';
+import React from 'react';
+import Context from '../Context';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'; 
 
 export default function Saida(){
+    const [value, setValue] = React.useState(0);
+    const [name, setName] = React.useState('');
+    const context = React.useContext(Context);
+    const navigate = useNavigate();
+    function sendTask(){
+        if(value !== 0 && name !== ''){
+            console.log(context.user);
+            let obj = {
+                email: context.user.email,
+                token: context.user.token,
+                name: name,
+                type: "saida",
+                value: value
+            }
+            let promisse = axios.post("http://localhost:5000/task", obj);
+            promisse.then(() => navigate('/wallet'));
+            promisse.catch(() => alert('Erro. Tente novamente!'));
+        } else {
+            alert('Preencha todos os campos');
+        }
+    }
     return(
         <S.Content>
             <h1>Nova saída</h1>
-            <input placeholder="Valor"></input>
-            <input placeholder="Descrição"></input>
-            <button>Salvar saída</button>
+            <input placeholder="Valor" type="number" value={value} onChange={e => setValue(e.target.value)}></input>
+            <input placeholder="Descrição" type="text" value={name} onChange={e => setName(e.target.value)}></input>
+            <button onClick={sendTask}>Salvar saída</button>
         </S.Content>
     )
 }
