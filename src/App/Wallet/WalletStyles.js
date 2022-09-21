b435e5a77205dfa943ca994e1671ee52f6c16cce
Matheus Mazetti <91391939+matheusmazetti@users.noreply.github.com>
2022-09-21 10:28:59 -0300

    Update WalletStyles.js

--- src/App/Wallet/WalletStyles.js
+++ src/App/Wallet/WalletStyles.js
@@ -125,7 +125,7 @@ export const Content = styled.div`
 
     .task{
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
         align-items: center;
     }
 
